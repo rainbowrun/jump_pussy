9960e55a76cea4093dc2bbdba070d9479e271674
Xiaopan Zhang <zhangxiaopan@gmail.com>
2019-10-26 15:53:55 -0700

    Initial version

---
+++ pussy.py
@@ -0,0 +1,123 @@
+# TODO: Add more swords.
+
+# TODO: Put the game under source control.
+# TODO: Build a binary which can be downloaded and run.
+# TODO: Make the sword looks better.
+# TODO: Add Bird
+# TODO: Add duck for Bird.
+# TODO: Add the your current time cycle.
+# TODO: Corp Pussy to remove unnecessary background.
+# TODO: When you die, make it visible on the screen
+# TODO: When you die, and can restart from the game.
+# TODO: Add 3 lifes.
+
+import pygame
+
+# The start position of a sword.
+sword_x = 400
+sword_y = 350
+sword_width = 20
+sword_height = 100
+sword_velocity = 20
+
+
+velocity = 20
+
+# Size of game window.
+max_width = 2000
+max_height = 500
+
+pygame.init()
+win = pygame.display.set_mode((max_width, max_height))
+pygame.display.set_caption("Pussy Jumping Game")
+
+# Load character.
+pussy = pygame.image.load('IMG-0102-new.JPG')
+print('Image is loaded successfully: %s' % pussy)
+print('Image size:', pussy.get_size())
+
+# Start position of the character.
+x = 100
+y = 400
+width, height = pussy.get_size()
+
+
+# Refresh time in millisecond
+refresh_time = 10
+
+# Sword move total period
+sword_move_total_period = 50
+current_cycle = 0
+
+
+# Whether in jump mode and jump cycle.
+is_jump = False
+jump_count = 10
+
+run = True
+while run:
+    pygame.time.delay(refresh_time)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    keys = pygame.key.get_pressed()
+    
+    # Pussy is dead.
+    if pygame.Rect(x, y, width, height).colliderect(pygame.Rect(sword_x, sword_y, sword_width, sword_height)):
+      continue
+
+    # Move the sword.
+    current_cycle += 1
+    if (current_cycle == sword_move_total_period):
+      current_cycle = 0
+      sword_x = sword_x - sword_velocity
+    
+    if keys[pygame.K_LEFT]:
+        x = x - velocity
+        if x <= 0:
+            x = 0
+    if keys[pygame.K_RIGHT]:
+        x = x + velocity
+        if x+width >= max_width:
+            x = max_width-width
+    if not is_jump:
+      if keys[pygame.K_SPACE]:
+          is_jump = True
+
+      # We should not allow the character to move up and down, which will just
+      # defeat our game purpose. But leave it for now to focus on the task at
+      # hand.
+      if keys[pygame.K_UP]:
+          y = y - velocity
+          if y <= 0:
+              y = 0
+      if keys[pygame.K_DOWN]:
+          y = y + velocity
+          if y+height >= max_height:
+              y = max_height-height
+
+    else:
+      if jump_count >= -10:
+         if jump_count > 0:
+           y = y - (jump_count ** 2) * 0.5
+         else:
+           y = y + (jump_count ** 2) * 0.5
+         jump_count -= 1
+      else:
+         is_jump = False
+         jump_count = 10
+        
+    win.fill((255, 255, 255))
+
+    # Draw sword.
+    pygame.draw.rect(win, (100, 100, 100),
+      (sword_x, sword_y, sword_width, sword_height))
+   
+    # Draw pussy.
+    win.blit(pussy, (x,y))
+
+    pygame.display.update()
+    
+pygame.quit()
