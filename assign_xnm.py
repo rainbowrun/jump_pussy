4593ad2e1366228ebf9ab69c11624e42d86faf3f
Xiaopan Zhang <zhangxiaopan@gmail.com>
2020-02-25 12:49:06 -0800

    X(N,M)/M! = S(N,M), good

--- assign_xnm.py
+++ assign_xnm.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
 #
-# Calculate identifiable objects, non-identifiable groups, each group has at
-# least 1 object.
+# Calculate identifiable objects, identifiable groups, each group has at least 1
+# object.
 #
-# This is like N books divided into M groups.
-
+# This is like N books given to M persons.
 
 import argparse
+import math
 
-# Key is (n, k) tuple, value is the corresponding stirling second kind number.
+# Key is (n, k) tuple, value is the corresponding number.
 TABLE = {}
 
-def StirlingSecondKind(num_objects, num_groups):
-  if num_objects == num_groups:
-    return 1
+def Choose(n, k):
+  return math.factorial(n) / (math.factorial(k) * math.factorial(n-k))
 
+def X(num_objects, num_groups):
   if num_groups == 1:
     return 1
 
   if (num_objects, num_groups) in TABLE:
     return TABLE[(num_objects, num_groups)]
 
-  result = StirlingSecondKind(num_objects-1, num_groups-1) + \
-           num_groups * StirlingSecondKind(num_objects-1, num_groups)
+  result = math.pow(num_groups, num_objects)
+
+  for i in range(1, num_groups):
+    result -= Choose(num_groups, i) * X(num_objects, num_groups - i)
+
   TABLE[(num_objects, num_groups)] = result
-  return result
+  return int(result)
 
 def main():
   parser = argparse.ArgumentParser(usage=__doc__)
@@ -41,9 +44,9 @@ def main():
 
   print(f'There are {len(TABLE)} intermediate result.')
   for key in TABLE:
-    print(f'\tS({key[0], key[1]}) = {TABLE[key]}')
+    print(f'\tX({key[0], key[1]}) = {TABLE[key]}')
 
-  print(StirlingSecondKind(FLAGS.num_objects, FLAGS.num_groups))
+  print(X(FLAGS.num_objects, FLAGS.num_groups))
 
 
 if __name__=='__main__':
